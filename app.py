b7fbd8e7036cf4319c947ea9e57268fa0dec051e
Zahraa Abdool Carrim <ZahraaAbdoolCarrim@outlook.com>
2026-01-30 12:22:22 +0000

    Implemented Dash Graph App

---
+++ app.py
@@ -0,0 +1,20 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash()
+df = pd.read_csv('data/daily_sales_data_improved.csv')
+fig = px.scatter(df, x='date', y='sales', color='region')
+dcc.Graph(figure=fig)
+
+app.layout = html.Div(children=[
+    html.H1(children='Daily Sales Data for Pink Morsels'),
+    dcc.Graph(
+        id='sales-scatter',
+        figure=fig
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
